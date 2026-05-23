a8a1d62537985537a4e2e4df4282b24e4a617ab0
DebadityaDas05 <debadityadas05@gmail.com>
2026-05-23 16:34:05 +0530

    fix: make 'student freelancers' text visible on all browsers

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,14 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 //import App from './App.jsx'
-import Layout from './components/Layout.jsx'
+//import Layout from './components/Layout.jsx'
 //import Home from './components/Home.jsx'
-//import Common from './pages/common.jsx'
+import Common from './pages/common.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <Layout />
+      <Common />
     </BrowserRouter>
   </StrictMode>,
 )
